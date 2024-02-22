848242025a42b918983ffe5f164c1988d0f0ef14
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-02-23 00:58:22 +0600

    image setup for eyes section

--- src/Components/Eyes.jsx
+++ src/Components/Eyes.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Eyes = () => {
   return (
-    <div>
+    <div className='w-full h-screen overflow-hidden'>
+        <div className='w-full h-full bg-cover bg-center bg-[url("https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-scaled.jpg")]'>
+
+        </div>
       
     </div>
   )
