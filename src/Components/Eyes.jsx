8e4479a1616b6adcd2d18f83d1abd13eef8c6c2d
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-02-23 01:18:08 +0600

    eyes created without animation

--- src/Components/Eyes.jsx
+++ src/Components/Eyes.jsx
@@ -3,8 +3,19 @@ import React from 'react'
 const Eyes = () => {
   return (
     <div className='w-full h-screen overflow-hidden'>
-        <div className='w-full h-full bg-cover bg-center bg-[url("https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-scaled.jpg")]'>
-
+        <div className='relative w-full h-full bg-cover bg-center bg-[url("https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-scaled.jpg")]'>
+            <div className='absolute flex  gap-10 top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]'>
+                <div className='w-[15vw] h-[15vw] rounded-full bg-zinc-100 flex items-center justify-center'>
+                    <div className='bg-zinc-900 w-2/3 h-2/3  rounded-full flex items-center justify-center'>
+                    <div className='bg-zinc-100 w-1/4 h-1/4  rounded-full '></div>
+                    </div>
+                </div>
+                <div className='w-[15vw] h-[15vw] rounded-full bg-zinc-100 flex justify-center items-center'>
+                    <div className='bg-zinc-900 w-2/3 h-2/3  rounded-full flex items-center justify-center'>
+                    <div className='bg-zinc-100 w-1/4 h-1/4  rounded-full '></div>
+                    </div>
+                </div>
+            </div>
         </div>
       
     </div>
