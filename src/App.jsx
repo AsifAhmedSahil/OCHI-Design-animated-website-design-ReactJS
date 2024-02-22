c3bcbf8e534505eed8eeb440bd411539e1141085
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-02-22 13:11:29 +0600

    marque without animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import LandingPage from './Components/LandingPage'
+import Marquee from './Components/Marquee'
 
 const App = () => {
   return (
-    <div className=' text-white'>
+    <div className=' text-white min-h-screen bg-zinc-900'>
       <Navbar/>
       <LandingPage/>
+      <Marquee/>
       
     </div>
   )
--- src/Components/Marquee.jsx
+++ src/Components/Marquee.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Marquee = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="w-full rounded-tl-3xl rounded-tr-3xl bg-[#004D43] py-20 ">
+      <div className="text flex gap-10 whitespace-nowrap overflow-hidden border-zinc-300 border-t-2 border-b-2">
+        <h1 className="text-[15vw] leading-none font-['Founders_Grotesk'] font-semibold uppercase -mb-2 pt-2 ">We are ochi</h1>
+        <h1 className="text-[15vw] leading-none font-['Founders_Grotesk'] font-semibold  uppercase -mb-2 pt-2">we are ochi</h1>
 
-export default Marquee
+      </div>
+    </div>
+  );
+};
+
+export default Marquee;
